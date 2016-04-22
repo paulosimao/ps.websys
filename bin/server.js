0dd259fd462575c22309b14f3a24d20c12b0ea4a
Paulo Simao <paulo.simao@mbr.com.br>
2016-04-22 14:51:03 -0300

    Initial commit

---
+++ bin/server.js
@@ -0,0 +1,6 @@
+/**
+ * Created by paulo.simao on 22/04/2016.
+ */
+var WSServer = require('../lib/WSServer');
+s            = new WSServer();
+s.load();
---
+++ libx/b/a.js
@@ -0,0 +1,4 @@
+/**
+ * Created by paulo.simao on 22/04/2016.
+ */
+console.log('Loading the file ' + __dirname + '/' + __filename);
