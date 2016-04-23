6aa8104a165aa68e2632cdd568baafa4792146aa
Paulo Simao <paulo.simao@mbr.com.br>
2016-04-22 22:45:29 -0300

    fixed lookups

--- libx/taskmgr/index.js
+++ libx/taskmgr/index.js
@@ -3,7 +3,7 @@
  */
 var express  = require('express');
 var mongoose = require('mongoose');
-var path = require('path')
+var path     = require('path')
 
 websys.app.use('/task', express.static(path.join(__dirname, 'webroot')));
 
@@ -11,8 +11,15 @@ var TaskSchema = new mongoose.Schema({
 	name    : String,
 	desc    : String,
 	dueby   : Date,
+	type    : mongoose.Schema.ObjectId,
 	complete: {type: Boolean, default: true}
 });
 
-var TaskModel = mongoose.model('Task', TaskSchema);
+var TaskModel      = mongoose.model('Task', TaskSchema);
+var TaskTypeSchema = new mongoose.Schema({
+	desc: String,
+});
+
+var TaskTypeModel = mongoose.model('TaskType', TaskSchema);
+websys.pm.add('tasktype', TaskTypeModel);
 websys.pm.add('task', TaskModel);
